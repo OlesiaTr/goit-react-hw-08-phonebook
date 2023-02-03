6c760b4751735fc689c8f4f055a7ff3af1f94445
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-02-03 20:42:42 +0200

    feat: add auth redux and refactor redux folder layout

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 // Components
 import { App } from 'components/App';
 
 // Styles
 import { GlobalStyle } from 'components/GlobalStyle';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-      <GlobalStyle />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <App />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
